c71ce249881432fbe586228a5041b689a027a133
Yang Yang <yangyang.jeff@bytedance.com>
2021-11-17 23:13:34 +0800

    feat: expose all interface of fund-tools

--- src/function/crawler.ts
+++ src/function/crawler.ts
@@ -1,12 +1,17 @@
 import {
-  Provide,
   Inject,
+  Provide,
+  Query,
   ServerlessTrigger,
   ServerlessTriggerType,
-  Query,
 } from '@midwayjs/decorator';
 import { Context } from '@midwayjs/faas';
-import { fetchUnitPriceByIdentifier } from 'fund-tools';
+import {
+  fetchSplitByIdentifier,
+  fetchUnitPriceByIdentifier,
+  fetchDividendByIdentifier,
+  fetchBasicInfoByIdentifier,
+} from 'fund-tools';
 
 @Provide()
 export class CrawlerHTTPService {
@@ -17,8 +22,31 @@ export class CrawlerHTTPService {
     path: '/fund/unitPrice',
     method: 'get',
   })
-  async handleHTTPEvent(@Query() identifier) {
-    const result = await fetchUnitPriceByIdentifier(identifier);
-    return result;
+  async fetchUnitPrice(@Query() identifier) {
+    return await fetchUnitPriceByIdentifier(identifier);
+  }
+
+  @ServerlessTrigger(ServerlessTriggerType.HTTP, {
+    path: '/fund/split',
+    method: 'get',
+  })
+  async fetchSplit(@Query() identifier) {
+    return await fetchSplitByIdentifier(identifier);
+  }
+
+  @ServerlessTrigger(ServerlessTriggerType.HTTP, {
+    path: '/fund/dividend',
+    method: 'get',
+  })
+  async fetchDividend(@Query() identifier) {
+    return await fetchDividendByIdentifier(identifier);
+  }
+
+  @ServerlessTrigger(ServerlessTriggerType.HTTP, {
+    path: '/fund/basicInfo',
+    method: 'get',
+  })
+  async fetchBasicInfo(@Query() identifier) {
+    return await fetchBasicInfoByIdentifier(identifier);
   }
 }
