164373e97e49ef6b1b342feeb72c5e28739cd3d7
Yang Yang <yangyang.jeff@bytedance.com>
2022-06-10 22:49:17 +0800

    feature: remove cache

--- src/service/crawler.ts
+++ src/service/crawler.ts
@@ -1,11 +1,11 @@
-import { Inject, Provide } from '@midwayjs/decorator';
+import { Provide } from '@midwayjs/decorator';
 import { InjectEntityModel } from '@midwayjs/typegoose';
 import { Model } from 'mongoose';
 import { Organization } from '../entity/organization';
 import { Dayjs } from 'dayjs';
 import { fetchBasicInfoByIdentifier, fetchDividendByIdentifier, fetchSplitByIdentifier, fetchUnitPriceByIdentifier } from 'fund-tools';
-import { getCacheFirstArrayResource, getCacheFirstObjectResource } from '../utils/response';
-import { CacheManager } from '@midwayjs/cache';
+// import { getCacheFirstArrayResource } from '../utils/response';
+// import { CacheManager } from '@midwayjs/cache';
 
 export interface PriceType {
   date: Dayjs;
@@ -32,34 +32,35 @@ export interface BasicInfoType {
 export class CrawlerService {
   @InjectEntityModel(Organization)
   organizationModel: Model<Organization>;
-  @Inject()
-  cache: CacheManager; // 依赖注入CacheManager
+  // @Inject()
+  // cache: CacheManager; // 依赖注入CacheManager
 
   public async fetchUnitPriceByIdentifier(identifier: string): Promise<Array<PriceType>> {
-    const result = (await getCacheFirstArrayResource(this.cache, `fetchUnitPriceByIdentifier${identifier}`, fetchUnitPriceByIdentifier(identifier), 3600)) as {
-      data: Array<PriceType>;
-      from: string;
-    };
-    return result.data;
+    // const result = (await getCacheFirstArrayResource(this.cache, `fetchUnitPriceByIdentifier${identifier}`, fetchUnitPriceByIdentifier(identifier), 3600)) as {
+    //   data: Array<PriceType>;
+    //   from: string;
+    // };
+    return await fetchUnitPriceByIdentifier(identifier);
   }
 
   public async fetchSplitByIdentifier(identifier: string): Promise<Array<SplitType>> {
-    const result = (await getCacheFirstArrayResource(this.cache, `fetchSplitByIdentifier${identifier}`, fetchSplitByIdentifier(identifier), 3600)) as {
-      data: Array<SplitType>;
-      from: string;
-    };
-    return result.data;
+    // const result = (await getCacheFirstArrayResource(this.cache, `fetchSplitByIdentifier${identifier}`, fetchSplitByIdentifier(identifier), 3600)) as {
+    //   data: Array<SplitType>;
+    //   from: string;
+    // };
+    return await fetchSplitByIdentifier(identifier);
   }
 
   public async fetchDividendByIdentifier(identifier: string): Promise<Array<DividendType>> {
-    const result = (await getCacheFirstArrayResource(this.cache, `fetchDividendByIdentifier${identifier}`, fetchDividendByIdentifier(identifier), 3600)) as {
-      data: Array<DividendType>;
-      from: string;
-    };
-    return result.data;
+    // const result = (await getCacheFirstArrayResource(this.cache, `fetchDividendByIdentifier${identifier}`, fetchDividendByIdentifier(identifier), 3600)) as {
+    //   data: Array<DividendType>;
+    //   from: string;
+    // };
+    return await fetchDividendByIdentifier(identifier);
   }
 
   public async fetchBasicInfoByIdentifier(identifier: string): Promise<BasicInfoType> {
-    return (await getCacheFirstObjectResource(this.cache, `fetchBasicInfoByIdentifier${identifier}`, fetchBasicInfoByIdentifier(identifier), 3600)) as BasicInfoType;
+    // return (await getCacheFirstObjectResource(this.cache, `fetchBasicInfoByIdentifier${identifier}`, fetchBasicInfoByIdentifier(identifier), 3600)) as BasicInfoType;
+    return await fetchBasicInfoByIdentifier(identifier);
   }
 }
--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,4 +1,4 @@
-import { CacheManager } from '@midwayjs/cache';
+// import { CacheManager } from '@midwayjs/cache';
 
 export const response200 = (data: any) => ({
   code: 200,
@@ -30,33 +30,33 @@ export const response500 = (message: string) => ({
   message,
 });
 
-export const getCacheFirstObjectResource = async (
-  cacheInstance: CacheManager,
-  cacheKey: string,
-  fetchRemote,
-  ttl: number
-  // eslint-disable-next-line @typescript-eslint/ban-types
-): Promise<object> => {
-  const cacheResult = await cacheInstance.get(cacheKey);
-  if (cacheResult && typeof cacheResult === 'object') {
-    return { ...cacheResult, from: 'cache' };
-  }
-  const remoteResult = await fetchRemote;
-  cacheInstance.set(cacheKey, remoteResult, { ttl });
-  return { ...remoteResult, from: 'remote' };
-};
-
-export const getCacheFirstArrayResource = async (
-  cacheInstance: CacheManager,
-  cacheKey: string,
-  fetchRemote,
-  ttl: number
-): Promise<{ data: any; from: string }> => {
-  const cacheResult = await cacheInstance.get(cacheKey);
-  if (cacheResult && typeof cacheResult === 'object') {
-    return { data: cacheResult, from: 'cache' };
-  }
-  const remoteResult = await fetchRemote;
-  cacheInstance.set(cacheKey, remoteResult, { ttl });
-  return { data: remoteResult, from: 'remote' };
-};
+// export const getCacheFirstObjectResource = async (
+//   cacheInstance: CacheManager,
+//   cacheKey: string,
+//   fetchRemote,
+//   ttl: number
+//   // eslint-disable-next-line @typescript-eslint/ban-types
+// ): Promise<object> => {
+//   const cacheResult = await cacheInstance.get(cacheKey);
+//   if (cacheResult && typeof cacheResult === 'object') {
+//     return { ...cacheResult, from: 'cache' };
+//   }
+//   const remoteResult = await fetchRemote;
+//   cacheInstance.set(cacheKey, remoteResult, { ttl });
+//   return { ...remoteResult, from: 'remote' };
+// };
+//
+// export const getCacheFirstArrayResource = async (
+//   cacheInstance: CacheManager,
+//   cacheKey: string,
+//   fetchRemote,
+//   ttl: number
+// ): Promise<{ data: any; from: string }> => {
+//   const cacheResult = await cacheInstance.get(cacheKey);
+//   if (cacheResult && typeof cacheResult === 'object') {
+//     return { data: cacheResult, from: 'cache' };
+//   }
+//   const remoteResult = await fetchRemote;
+//   cacheInstance.set(cacheKey, remoteResult, { ttl });
+//   return { data: remoteResult, from: 'remote' };
+// };
